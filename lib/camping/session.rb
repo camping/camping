6865f642c8430577f7f0bc7fb39189c43af9d40a
_why <why@whytheluckystiff.net>
2006-03-07 04:52:16 +0000

    * lib/camping/session.rb: session was broken, thanks to Ken Allen for noticing.

--- lib/camping/session.rb
+++ lib/camping/session.rb
@@ -8,21 +8,30 @@ class Session < Base
         self.ivars[k] rescue nil
     end
 
-    RAND_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-                 "0123456789" +
-                 "abcdefghijklmnopqrstuvwxyz" 
+    RAND_CHARS = [*'A'..'Z'] + [*'0'..'9'] + [*'a'..'z']
 
-    def self.generate
+    def self.generate cookies
         rand_max = RAND_CHARS.size
         sid = (0..32).inject("") { |ret,_| ret << RAND_CHARS[rand(rand_max)] }
-        Session.create :hashid => sid, :ivars => Camping::H[]
+        sess = Session.create :hashid => sid, :ivars => Camping::H[]
+        cookies.camping_sid = sess.hashid
+        sess
+    end
+    def self.persist cookies
+        if cookies.camping_sid
+            session = Camping::Models::Session.find_by_hashid cookies.camping_sid
+        end
+        unless session
+            session = Camping::Models::Session.generate cookies
+        end
+        session
     end
     def self.create_schema
         unless table_exists?
             ActiveRecord::Schema.define do
                 create_table :sessions, :force => true do |t|
                     t.column :id,          :integer, :null => false
-                    t.column :hashid,      :string,  :limit => 16
+                    t.column :hashid,      :string,  :limit => 32
                     t.column :created_at,  :datetime
                     t.column :ivars,       :text
                 end
@@ -36,20 +45,15 @@ end
 module Camping
 module Session
     def service(*a)
-        if @cookies.camping_sid
-            session = Camping::Models::Session.find_by_hashid @cookies.camping_sid
-        end
-        unless session
-            session = Camping::Models::Session.generate
-            @cookies.camping_sid = session.hashid
-        end
+        session = Camping::Models::Session.persist @cookies
         app = self.class.name.gsub(/^(\w+)::.+$/, '\1')
         @state = (session.ivars[app] ||= Camping::H[])
         hash_before = Marshal.dump(@state).hash
         s = super(*a)
         if session
-            hash_after = Marshal.dump(session.ivars[app]).hash
+            hash_after = Marshal.dump(@state).hash
             unless hash_before == hash_after
+                session.ivars[app] = @state
                 session.save
             end
         end
