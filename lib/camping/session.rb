2be06a343ee49a2de072cf3045d6d05d732de0ba
Magnus Holm <judofyr@gmail.com>
2010-04-24 19:19:35 +0200

    Camping::Session: Now uses C.options

--- lib/camping/session.rb
+++ lib/camping/session.rb
@@ -3,16 +3,16 @@ module Camping
   #
   # To get sessions working for your application:
   # 1. <tt>require 'camping/session'</tt>
-  # 2. Mixin the module: <tt>include Camping::Session</tt>
-  # 3. Define a secret (and keep it secret): <tt>secret "SECRET!"</tt>
+  # 2. Define a secret (and keep it secret): <tt>set :secret, "SECRET!"</tt>
+  # 3. Mixin the module: <tt>include Camping::Session</tt>
   # 4. Throughout your application, use the <tt>@state</tt> var like a hash
   #    to store your application's data.
   #
   #   require 'camping/session'    # 1
   #   
   #   module Nuts
-  #     include Camping::Session   # 2
-  #     secret "Oh yeah!"          # 3
+  #     set :secret, "Oh yeah!"    # 2
+  #     include Camping::Session   # 3
   #   end
   #
   # == Other backends
@@ -27,9 +27,8 @@ module Camping
   module Session
     def self.included(app)
       key    = "#{app}.state".downcase
-      secret = [__FILE__, File.mtime(__FILE__)].join(":")
+      secret = app.options[:secret] || [__FILE__, File.mtime(__FILE__)].join(":")
       
-      app.meta_def(:secret) { |val| secret.replace(val) } 
       app.use Rack::Session::Cookie, :key => key, :secret => secret
     end
   end
