2448775ffe20bdd6a95772c1281a815224c80106
Magnus Holm <judofyr@gmail.com>
2011-08-24 09:57:05 +0200

    Lock Rake version

--- constants.rb
+++ constants.rb
@@ -24,7 +24,7 @@ def spec
     s.executables = ['camping']
 
     s.add_dependency('rack', '>=1.0')
-    s.add_development_dependency('rake')
+    s.add_development_dependency('rake', '=0.8.7')
     s.add_development_dependency('rack-test')
     s.required_ruby_version = '>= 1.8.2'
 
