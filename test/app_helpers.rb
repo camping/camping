7aa0e1fa934806f964ad120c1b4bb21783c7e008
Magnus Holm <judofyr@gmail.com>
2011-10-05 09:38:45 +0200

    Add failing test for controllers/models in helpers

---
+++ test/app_helpers.rb
@@ -0,0 +1,47 @@
+require 'test_helper'
+require 'camping'
+
+Camping.goes :Helpers
+
+module Helpers::Helpers
+  def frontpage
+    R(Index)
+  end
+
+  def current_user
+    User.new
+  end
+end
+
+module Helpers::Models
+  class User
+    def name
+      'Bob'
+    end
+  end
+end
+
+module Helpers::Controllers
+  class Index
+    def get
+      current_user.name
+    end
+  end
+
+  class Users
+    def get
+      frontpage
+    end
+  end
+end
+
+class Helpers::Test < TestCase
+  def test_inline
+    get '/'
+    assert_body "Bob"
+
+    get '/users'
+    assert_body "/"
+  end
+end
+
