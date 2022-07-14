5b594a150423187b61624c2a79785587cd406c05
karloscarweber <me@kow.fm>
2022-07-14 17:46:40 -0600

    Camping and Camping-unabridged are now synchronized again.

--- lib/camping.rb
+++ lib/camping.rb
@@ -38,7 +38,7 @@ def v;@r.map(&:urls);end;def D p,m,e;p='/'if
 !p||!p[0];(a=O[:_t].find{|n,_|n==p}) and return [I,:serve,*a]
 @r.map{|k|k.urls.map{|x|return(k.method_defined? m)?[k,m,*$~[1..-1].map{|x|U.unescape x}]:
 [I, 'r501',m]if p=~/^#{x}\/?$/}};[I,'r404',p] end;N=H.new{|_,x|x.downcase}.
-merge!("N"=>'(\d+)',"X"=>'([^/]+)',"Index"=>'');def M;def M;end;constants.
+merge!("N"=>'(\d+)',"X"=>'([^/]+)',"Index"=>'');def M;constants.
 map{|c|k=const_get(c);k.send:include,C,X,Base,Helpers,Models
 @r=[k]+@r if @r-[k]==@r;k.d(:urls){["/#{c.to_s.scan(/.[^A-Z]*/).map(&
 N.method(:[]))*'/'}"]}if !k.respond_to?:urls}end end;I=R()end;X=
