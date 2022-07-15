e61e9d997e03c7ac3aef8bb2ef35f79e0c318742
karloscarweber <me@kow.fm>
2022-07-14 18:26:23 -0600

    Refine route command line test and code to make it more consistent.

--- test/app_route_generating.rb
+++ test/app_route_generating.rb
@@ -31,10 +31,12 @@ class Routes::Test < TestCase
   end
 
   def test_routes_helper
-    routes = Camping::Commands.routes Camping::Apps[8]
+    #Make sure to call the route maker.
+    Camping.routes
+    routes = Camping::Commands.routes Camping::Apps[8], false
     assert_equal routes.count, 3, "Routes are not numbered correctly"
-    assert routes[0].to_s == "Index GET      /index", "Routes do not include /, #{routes[0].to_s}"
-    assert routes[1].to_s == "PageX GET      /page/:String", "Routes do not include /, #{routes[0].to_s}"
-    assert routes[2].to_s == "PageX POST     /page/:String", "Routes do not include /, #{routes[0].to_s}"
+    assert routes[0].to_s == "Index GET      /index", "Routes do not include: #{routes[0].to_s}"
+    assert routes[1].to_s == "PageX GET      /page/:String", "Routes do not include: #{routes[1].to_s}"
+    assert routes[2].to_s == "PageX POST     /page/:String", "Routes do not include: #{routes[2].to_s}"
   end
 end
