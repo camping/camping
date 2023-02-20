8b336aec3285a4425aedcd1e59c768b8b34af29c
karloscarweber <me@kow.fm>
2023-02-20 02:22:04 -0700

    Add more assertions to the config settings test thing.

--- test/app_config.rb
+++ test/app_config.rb
@@ -17,11 +17,14 @@ class Config::Test < TestCase
     super
   end
 
-  def test_something
+  def test_config
     assert @options.has_key? :hostname
     assert @options.has_key? :friends
     assert_equal @options[:friends].first, "_why", "_why isn't here?"
     assert_equal @options[:friends].length, 3, "Where are all our friends?"
+    assert_equal @options.has_key?(:database), true, "By Golly the Database settings are missing."
+    assert_equal @options[:database].length, 4, "We're missing some database settings."
+    assert_equal @options[:database][:adapter], "sqlite3", "sqlite is missing. Not good."
   end
 
 end
