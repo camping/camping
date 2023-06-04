19cab4e1fc28983cd8a58d312c54b6d5a9d2ed92
karloscarweber <me@kow.fm>
2023-06-04 15:24:01 -0600

    Added new stuff to test the loader, to make sure it loads and reopens classes as expected.

---
+++ test/apps/loader/apps/donuts.rb
@@ -0,0 +1,15 @@
+module Donuts
+  module Controllers
+    class Index
+      def get
+        "To do things, i need help, friends!!! how are ? Are you goood? What else is new?"
+      end
+    end
+
+    class People
+      def get
+        "People are great am I right?"
+      end
+    end
+  end
+end
--- test/apps/loader/camp.rb
+++ test/apps/loader/camp.rb
@@ -3,3 +3,33 @@ require 'camping'
 Camping.goes :Loader
 
 $LOAD_PATH << File.dirname(__FILE__)
+
+Camping.goes :Donuts
+
+module Donuts
+  module Controllers
+    class Index
+      def get
+        render :index
+      end
+    end
+    class Post
+      def get
+        render :post
+      end
+    end
+  end
+
+  # how to add a layout to every page
+  module Views
+
+    def index
+      "_why"
+    end
+
+    def post
+      "_why"
+    end
+
+  end
+end
