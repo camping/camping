d42eaec97d9190927d3d58b3e7fab26c30ffeed2
karloscarweber <me@kow.fm>
2024-04-16 03:53:40 -0600

    Add Logger to Camping.

--- lib/camping/gear/firewatch.rb
+++ lib/camping/gear/firewatch.rb
@@ -1,10 +1,44 @@
 require 'rack'
 require 'rack/utils'
 require 'rack/common_logger'
+require 'dry/logger'
+
+
+# Firewatch is Camping's logger.
+# It wraps Rack::CommonLogger, and gives a mechanism to Redirect logs.
+module Camping
+  class Firewatch < Rack::CommonLogger
+
+    class << self
+      def logger
+        @logger ||= default_logger
+      end
+      def logger=(new_logger)
+        @logger = new_logger
+      end
+      def default_logger
+        Dry.Logger(:Camping, template: default_template).add_backend(stream: "logs/development.log")
+      end
+      def default_template
+        "[<green>%<severity>s</green> - %<time>s] %<message>s"
+      end
+    end
+
+    # +logger+ can be any object that supports the +write+ or +<<+ methods,
+    # which includes the standard library Logger.  These methods are called
+    # with a single string argument, the log message.
+    # If +logger+ is nil, Firewatch(CommonLogger) will fall back <tt>env['rack.errors']</tt>.
+    def initialize(app, logger = nil)
+      @app = app
+      @logger = Camping::Firewatch.logger = logger.nil? ? Camping::Firewatch.default_logger : logger
+    end
+
+  end
+end
 
 module Gear
 
-  # Firewatch Gear gives us helper methods to access the logger, log stuff,
+  # Fire watch Gear gives us helper methods to access the logger, log stuff,
   # and do other shenanigans
   module Firewatch
 
@@ -12,8 +46,6 @@ module Gear
 
       def included(mod)
         mod::Helpers.include(HelperMethods)
-        # Actually we want to hijack env['rack.errors to be our logger instance']
-        # mod.extend(ClassMethods)
       end
 
       # required for compliance reasons
@@ -32,79 +64,11 @@ module Gear
       # @message: String, Default: nil, An optional value used to directly log
       # rather than requesting the logger
       def log(message = nil)
-        return Camping::Firewatch.logger unless !message.nil?
-        Camping::Firewatch.logger.info message
-        # Camping::Firewatch.logger.info(message)
-        # either grabs the rack.logger or makes a new logger and returns it.
-        # Actually we want to hijack env['rack.errors to be our logger instance']
-        # @env['rack.logger'] ||= ::Logger.new(env['rack.errors'])
+        return logger unless !message.nil?
+        logger.info message
       end
     end
 
-    # module ClassMethods
-    #   def logger=(new_logger)
-    #   end
-    # end
-
   end
-#
-#   # Inherit from Rack Logger to get things started.
-#   class Logger # < Rack::CommonLogger
-#
-#     Utils = Rack::Utils
-#
-#     def initialize(app, level = ::Logger::INFO)
-#       @app, @level = app, level
-#     end
-#
-#     def call(env)
-#       began_at = Utils.clock_time
-#       # sets up a new logger using rack.errors as the error stream
-#       logger = ::Logger.new(env['rack.errors'])
-#       logger.level = @level
-#
-#       # drops the logger into the rack.logger env spot so that we can grab it
-#       # down the middleware chain.
-#       env['rack.logger'] = logger
-#       @app.call(env)
-#     ensure
-#       logger.close
-#     end
-#
-#   end
-#
-# end
-#
-# # Sets up a default logger file thing
-# module Camping
-#
-#   # We make our own logger named firewatch.
-#   # this is like, middleware and stuff.
-#   class Firewatch
-#
-#     @logger = $stdout
-#
-#
-#     def initialize()
-#     end
-#
-#
-#
-#     class << self
-#
-#       @logger = nil
-#
-#       # returns a new logger a specific app, or just the default logger
-#       def book
-#         return @logger unless @logger == nil
-#         file = 'log/camp.log'
-#         Dir.mkdir 'log' unless Dir.exist? 'log'
-#         File.open(file, 'w') unless File.exist? file
-#         @logger ||= Logger.new(file)
-#       end
-#
-#     end
-#
-#   end
-#
+
 end
--- test/app_logger.rb
+++ test/app_logger.rb
@@ -1,15 +1,12 @@
 require 'test_helper'
 require 'camping'
-require 'camping/firewatch'
 
 Camping.goes :Loggy
 
-# Loggy.use ::Gear::Logger, Loggy
 Loggy.pack Gear::Firewatch
 
-Loggy.set :logger, {file: 'log/development.log'}
-
 module Loggy::Controllers
+
   class Index
     def get
       # @env['rack.errors'] = StringIO.new
@@ -21,13 +18,52 @@ module Loggy::Controllers
 end
 
 class Loggy::Test < TestCase
+
+  def logs
+    File.read Dir["./**/logs/development.log"].first
+  end
+
+  def after_all
+    `rm -rf logs` if File.exist?('logs/development.log')
+    `rm -rf logs` if File.exist?('logs/formatter.log')
+    `rm -rf logs` if File.exist?('logs/production.log')
+    super
+  end
+
   def test_logging
     get '/'
-    # puts Dir["./logs/development.log"]
+    assert_log "Program Started"
     assert_log "INFO"
-    # assert_log %r{\[INFO}
-    # assert_log %r{Program Started}
-    # assert_log %r{\[WARN}
-    # assert_log %r{Nothing to do}
   end
+
+  def test_log_levels
+    get '/'
+    assert(/(INFO).*Program Started$/.match?(logs), "Log level of INFO not found.")
+    assert(/(WARN).*Nothing to do!$/.match?(logs), "Log level of WARN not found.")
+  end
+
+  def test_log_on_error
+    get '/'
+    assert_raises {
+      raise "[Error]: There was a big error and I don't like it."
+    }
+  end
+
+  def test_change_log_location
+    Camping::Firewatch.logger = Dry.Logger(:Camping, template: Camping::Firewatch.default_template).add_backend(stream: "logs/production.log")
+    puts Camping::Firewatch.logger
+    get '/'
+    lags = File.read Dir["./**/logs/production.log"].first
+    assert(/(INFO).*Program Started$/.match?(lags), "Log level of INFO not found.")
+
+    # the end of the test means we set it back.
+    Camping::Firewatch.logger = Dry.Logger(:Camping, template: Camping::Firewatch.default_template).add_backend(stream: "logs/development.log")
+  end
+
+  # def test_changing_loggging_formatter
+  #   logger = Dry.Logger(:Camping, formatter: :rack).add_backend(stream: "logs/formatter.log")
+  #   get '/'
+  #   assert(/(INFO).*Program Started$/.match?(logs), "Log level of INFO not found.")
+  # end
+
 end
